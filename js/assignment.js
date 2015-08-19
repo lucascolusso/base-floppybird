24643bc11cd2ca8f48613864ac1c3d36f723c40a
lucascolusso <lucascolusso@gmail.com>
2015-08-19 00:22:29 -0700

    some work on the conditions

--- js/assignment.js
+++ js/assignment.js
@@ -18,74 +18,117 @@ function showVariables() {
   alert("variables: " + JSON.stringify(variables));
 }
 
-// feedback routing
+// feedback routing 2nd experiment
 function assignment () {
   //showVariables();
 
   switch(condition) {
 
-  // max - leader
+  // scores
     case 0:
       conditions.comparison.leader();
       userCondition = conditions.nonSkewed;
       break;
     case 1:
       //conditions.fix();
-      conditions.comparison.leader();
-      userCondition = conditions.skewed;
+      conditions.comparison.high();
+      userCondition = conditions.nonskewed;
       break;
     case 2:
-      conditions.comparison.leader();
+      conditions.comparison.median();
       userCondition = conditions.nonSkewed;
-      leaderBarName = 'Similar player';
       document.getElementById("target-txt").textContent = "Highest score among similar players:";
       break;
-    // case 3:
-    //   conditions.comparison.leader();
-    //   userCondition = conditions.nonSkewed;
-    //   document.getElementById("target-txt").textContent = "It took the leader 2 rounds to accomplish this score.";
-    //   break;
 
-    // highs
+
+    // rounds
     case 3:
-      conditions.comparison.high();
+      conditions.comparison.leader();
       userCondition = conditions.nonSkewed;
+      parameter = 'Rounds';
       break;
     case 4:
       conditions.comparison.high();
       userCondition = conditions.skewed;
+      parameter = 'Rounds';
       break;
     case 5:
-      conditions.comparison.high();
+      conditions.comparison.median();
       userCondition = conditions.nonSkewed;
-      leaderBarName = 'Similar player';
+      parameter = 'Rounds';
       document.getElementById("target-txt").textContent = "Highest score among similar players:";
       break;
-    // case 7:
-    //   conditions.comparison.high();
-    //   userCondition = conditions.nonSkewed;
-    //   document.getElementById("target-txt").textContent = "It took the leader 6 rounds to accomplish this score.";
-    //   break;
-
-    // median
-    case 6:
-      conditions.comparison.median();
-      userCondition = conditions.nonSkewed;
-      break;
-    case 7:
-      conditions.comparison.median();
-      userCondition = conditions.skewed;
-      break;
-    case 8:
-      conditions.comparison.median();
-      userCondition = conditions.nonSkewed;
-      leaderBarName = 'Similar player';
-      document.getElementById("target-txt").textContent = "Highest score among similar players";
-      break;
-    // case 11:
-    //   conditions.comparison.median();
-    //   userCondition = conditions.nonSkewed;
-    //   document.getElementById("target-txt").textContent = "It took the leader 10 rounds to accomplish this score.";
-    //   break;
   }
 }
+
+// // feedback routing 1st experiment
+// function assignment () {
+//   //showVariables();
+//
+//   switch(condition) {
+//
+//   // max - leader
+//     case 0:
+//       conditions.comparison.leader();
+//       userCondition = conditions.nonSkewed;
+//       break;
+//     case 1:
+//       //conditions.fix();
+//       conditions.comparison.leader();
+//       userCondition = conditions.skewed;
+//       break;
+//     case 2:
+//       conditions.comparison.leader();
+//       userCondition = conditions.nonSkewed;
+//       leaderBarName = 'Similar player';
+//       document.getElementById("target-txt").textContent = "Highest score among similar players:";
+//       break;
+//     // case 3:
+//     //   conditions.comparison.leader();
+//     //   userCondition = conditions.nonSkewed;
+//     //   document.getElementById("target-txt").textContent = "It took the leader 2 rounds to accomplish this score.";
+//     //   break;
+//
+//     // highs
+//     case 3:
+//       conditions.comparison.high();
+//       userCondition = conditions.nonSkewed;
+//       break;
+//     case 4:
+//       conditions.comparison.high();
+//       userCondition = conditions.skewed;
+//       break;
+//     case 5:
+//       conditions.comparison.high();
+//       userCondition = conditions.nonSkewed;
+//       leaderBarName = 'Similar player';
+//       document.getElementById("target-txt").textContent = "Highest score among similar players:";
+//       break;
+//     // case 7:
+//     //   conditions.comparison.high();
+//     //   userCondition = conditions.nonSkewed;
+//     //   document.getElementById("target-txt").textContent = "It took the leader 6 rounds to accomplish this score.";
+//     //   break;
+//
+//     // median
+//     case 6:
+//       conditions.comparison.median();
+//       userCondition = conditions.nonSkewed;
+//       break;
+//     case 7:
+//       conditions.comparison.median();
+//       userCondition = conditions.skewed;
+//       break;
+//     case 8:
+//       conditions.comparison.median();
+//       userCondition = conditions.nonSkewed;
+//       leaderBarName = 'Similar player';
+//       document.getElementById("target-txt").textContent = "Highest score among similar players";
+//       break;
+//     // case 11:
+//     //   conditions.comparison.median();
+//     //   userCondition = conditions.nonSkewed;
+//     //   document.getElementById("target-txt").textContent = "It took the leader 10 rounds to accomplish this score.";
+//     //   break;
+//   }
+// }
